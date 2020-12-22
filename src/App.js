9edf1c468017addb1accef6036ab4f90621a9bab
IgorGarciaCosta <igonildo7@gmail.com>
2020-12-22 17:29:36 -0300

    botão de adicionar item digitado na lista

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]) ;
+  const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });//passo um objeto rpo userState
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    if (!name) {//se nada foi digitado, dá um alerta
+      //display alert
+    }
+
+    else if (name && isEditing) {
+      //deal with edit
+    }
+    else {// se tudo der certo, adiciona o item
+      //show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName('')//isso limpa o input dps de dar submit
+    }
   }
 
   return <section className="section-center">
@@ -20,19 +32,20 @@ function App() {
       {alert.show && <Alert />}
       <h3>Caixa da Mercearia</h3>
       <div className="form-control">
-        <input type="text" className="grocery" placeholder="ex. batata" value = {name} onChange={(e)=>setName(e.target.value)}/>
+        <input type="text" className="grocery" placeholder="ex. batata" value={name} onChange={(e) => setName(e.target.value)} />
         <button type="submit" className="submit-btn">
-          {isEditing?'edit':'submit'}
+          {isEditing ? 'edit' : 'submit'}
         </button>
       </div>
 
     </form>
-    <div className="grocery-container">
-      <List />
-      <button className="clear-btn">
-        limpar itens
+    {list.length > 0 &&
+      (<div className="grocery-container">
+        <List items={list} />{/*passa a lista como um prop*/}
+        <button className="clear-btn">
+          limpar itens
       </button>
-    </div>
+      </div>)}
   </section>
 }
 
